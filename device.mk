21d4ce3687ea2c28290a63641623ea317cdb76ec
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-03-07 11:10:09 +0000

    payon: telephony: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    https://github.com/PixelExp-Project/vendor_aosp/commit/c3b9f5c170c794966bb941282c3f9f18baa6279a
    
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>
    Change-Id: I88ddeb57aa54e4fad88c3c9cba331dbbd7b4bd59
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_PACKAGES += \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0:64
 
+# Call recording for Google Dialer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
